ad920e4d76569f8d3418742789d91f99eac7d752
FabianGroger96 <fabian.groeger@bluewin.ch>
2019-08-29 15:32:32 +0200

    refactoring for pip upload

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='docx2python',
-    version='1.1',
-    author="Shay Hill",
-    author_email="shay_public@hotmail.com",
-    description="Extract content from docx files",
+    name='docx2python-kryon',
+    version='1.0.0',
+    author="Fabian Gröger",
+    author_email="fabian.groeger@bluein.ch",
+    description="Extract content from docx files. Edited for Kryon Studio (RPA).",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ShayHill/docx2python",
+    url="https://github.com/FabianGroeger96/docx2python",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
